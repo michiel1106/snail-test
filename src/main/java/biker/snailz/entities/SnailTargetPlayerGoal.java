abc05470ece5d619dd7ac4d4c954890b54621948
michiel1106 <michielimmink@gmail.com>
2024-11-18 20:40:09 +0100

    trying to make ai better

--- src/main/java/biker/snailz/entities/SnailTargetPlayerGoal.java
+++ src/main/java/biker/snailz/entities/SnailTargetPlayerGoal.java
@@ -3,13 +3,14 @@ package biker.snailz.entities;
 import net.minecraft.entity.MovementType;
 import net.minecraft.entity.ai.goal.Goal;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.ai.pathing.Path;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
 public class SnailTargetPlayerGoal extends Goal {
-
+    private Path path;
     private final SnailEntity snail;
     private Vec3d targetPlayer;
 
@@ -21,7 +22,11 @@ public class SnailTargetPlayerGoal extends Goal {
     @Override
     public boolean canStart() {
         targetPlayer = snail.getTargetPlayer(); // This will now return the position of the resolved player
+        Vec3d targetPos = snail.getTargetPlayer();
+
+
         return targetPlayer != null;
+
     }
 
     @Override
@@ -30,7 +35,12 @@ public class SnailTargetPlayerGoal extends Goal {
         if (targetPos == null) return;
 
 
-            snail.getNavigation().startMovingTo(targetPos.x, targetPos.y, targetPos.z, SnailEntity.WALKING_SPEED);
+        path = this.snail.getNavigation().findPathTo(targetPos.x, targetPos.y, targetPos.z, 0);
+        snail.getNavigation().startMovingAlong(path, 0.3);
+
+
+        //snail.getNavigation().startMovingTo(targetPos.x, targetPos.y, targetPos.z, 0, 0.3);
+        //snail.getNavigation().startMovingTo(targetPos.x, targetPos.y, targetPos.z, SnailEntity.WALKING_SPEED);
 
 
     }
